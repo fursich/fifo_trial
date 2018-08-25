96be7fd592bc299ab57d084198dc0ce63cdbe3c4
Koji Onishi <fursich0@gmail.com>
2018-08-25 18:24:01 +0900

    refactor tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,8 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
+Dir[ File.expand_path('shared_examples/**/*.rb', __dir__)].each { |f| require f }
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
