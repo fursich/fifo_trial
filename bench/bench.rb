41f901b35ee23fa83bbc75e0409165ac6846aaf1
Koji Onishi <fursich0@gmail.com>
2018-08-26 03:14:11 +0900

    update benchmark

--- bench/bench.rb
+++ bench/bench.rb
@@ -1,29 +1,52 @@
 require 'benchmark'
-require_relative '../lib/lifo.bundle'
+require_relative '../bin/lifo.bundle'
+require_relative '../bin/fifo.bundle'
 require_relative '../lib/rb_lifo.rb'
+require_relative '../lib/rb_fifo.rb'
 
 BENCHMARK_COUNT = 100
 UNIT_ITERATION = 100000
 
 Benchmark.bm BENCHMARK_COUNT do |r|  
   r.report 'LIFO (c-ext)' do
-    f = Lifo.new
+    l = Lifo.new
     UNIT_ITERATION.times do |n|
-      f.push(n)
+      l.push(n)
     end
     UNIT_ITERATION.times do |n|
-      f.pop
+      l.pop
     end
   end
 
   r.report 'LIFO (ruby)' do
-    f = RBLifo.new
+    l = RBLifo.new
     UNIT_ITERATION.times do |n|
-      f.push(n)
+      l.push(n)
     end
     UNIT_ITERATION.times do |n|
-      f.pop
+      l.pop
     end
   end
+
+  r.report 'FIFO (c-ext)' do
+    f = Fifo.new
+    UNIT_ITERATION.times do |n|
+      f.enqueue(n)
+    end
+    UNIT_ITERATION.times do |n|
+      f.dequeue
+    end
+  end
+
+  r.report 'FIFO (ruby)' do
+    f = RBFifo.new
+    UNIT_ITERATION.times do |n|
+      f.enqueue(n)
+    end
+    UNIT_ITERATION.times do |n|
+      f.dequeue
+    end
+  end
+
 end
 
